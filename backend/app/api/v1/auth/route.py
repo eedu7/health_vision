4d3d0eb17169d0c0ce67f843589a51213deb77ad
eedu7 <mueedahmad067@gmail.com>
2025-11-04 09:48:35 +0500

    Added some files

--- backend/app/api/v1/auth/route.py
+++ backend/app/api/v1/auth/route.py
@@ -3,12 +3,12 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from app.core.dependencies import get_auth_service
-from app.schemas.users import UserIn
+from app.schemas.auth import UserCreate
 from app.services import AuthService
 
 router = APIRouter()
 
 
 @router.post("/register")
-async def register(data: UserIn, service: Annotated[AuthService, Depends(get_auth_service)]):
+async def register(data: UserCreate, service: Annotated[AuthService, Depends(get_auth_service)]):
     return {"message": "User registered successfully"}
--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,6 +1,21 @@
-from pydantic import BaseModel, EmailStr, Field
+import re
+
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 
 class UserCreate(BaseModel):
     email: EmailStr = Field(..., title="Email", description="The email address of the user")
-    password: str = Field(..., title="Password", description="The password of the user")
+    password: str = Field(..., min_length=16, max_length=64, title="Password", description="The password of the user")
+
+    @field_validator("password")
+    def validate_password(cls, value: str) -> str:
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[0-9]", value):
+            raise ValueError("Password must contain at least one digit.")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
+            raise ValueError("Password must contain at least one special character.")
+
+        return value
